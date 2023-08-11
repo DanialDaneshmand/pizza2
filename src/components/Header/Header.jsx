b571254065b9b031d46ea15eb32fdb03743d63fe
danial daneshmand <daniald11111377@gmail.com>
2023-08-11 00:11:23 -0700

    home page added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React, { useState } from "react";
+import logo from "../../assets/images/res-logo.png";
+import { Link, NavLink } from "react-router-dom";
+import { FaShoppingCart } from "react-icons/fa";
+import { FaBars } from "react-icons/fa";
+import { FaUserPlus } from "react-icons/fa";
 
-function Header() {
+const items = [
+  { display: "خانه ", to: "/" },
+  { display: "محصولات ", to: "/allFoods" },
+  { display: "سبد خرید", to: "/cart" },
+  { display: "تماس با ما", to: "/contact" },
+];
+const Header = () => {
+  const [active, setActive] = useState(true);
   return (
-    <div>Header</div>
-  )
-}
+    <header className="flex justify-between px-4 md:px-0 md:justify-around items-center shadow-lg py-2 bg-[#eee]">
+      {/* this section is left logo */}
+      <div className="flex flex-col items-center">
+        <img src={logo} alt="logo" className="w-12" />
+        <p className="text-l font-bold">Tasty Treat</p>
+      </div>
+      {/* middle section */}
+      <div
+        onClick={() => setActive(true)}
+        className={`flex justify-between  md:w-5/12 lg:w-4/12 ${
+          active ? "md:block hidden" : "menu"
+        } `}
+      >
+        <div className={`${active ? "md:block hidden" : "navigation"}`}>
+          {items.map((item) => (
+            <NavLink
+              onClick={() => {
+                setActive(true);
+              }}
+              to={item.to}
+              className={(active) =>
+                active.isActive
+                  ? "font-bold text-orange-600 mr-0 md:mr-4 mt-4 md:mt-0"
+                  : "text-slate-900 hover:text-orange-600 font-bold  mr-0 md:mr-4 mt-4 md:mt-0"
+              }
+            >
+              {item.display}
+            </NavLink>
+          ))}
+        </div>
+      </div>
+      {/* right section of navigation */}
+      <div className="flex text-slate-900 ">
+        <Link className="flex mr-4 relative" to="/cart">
+          <FaShoppingCart />
+          <span className="cart-span text-orange-600 font-bold">2</span>
+        </Link>
+        <Link className="mr-2" to="/login">
+          <FaUserPlus />
+        </Link>
+        <span className="md:hidden" onClick={() => setActive(!active)}>
+          <FaBars />
+        </span>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
-
+import React from "react";
+import heroImg from "../assets/images/hero.png";
+import { FaCar } from "react-icons/fa";
+import { FaShieldHalved } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 function Home() {
   return (
-    <div>Kome</div>
-  )
+    <section className="grid grid-cols-2">
+      <div
+        style={{ direction: "rtl" }}
+        className="flex flex-col gap-8 px-16 py-32 text-slate-900"
+      >
+        <p className="text-2xl font-bold">راهی اسان برای سفارش دادن</p>
+        <p className="text-4xl font-bold">
+          <span className="text-orange-600">گرسنه ای ؟</span>یه لحظه صبر کن غذا{" "}
+          <span className="text-orange-600">پشت دره</span>
+        </p>
+        <p className=" text-gray-500">
+          لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
+          استفاده از طراحان  است، چاپگرها و متون بلکه  از طراحان گرافیک است از طراحان گرافیک است
+        </p>
+        <div className="flex gap-8 items-center">
+          <button className="font-bold hover:bg-slate-800 rounded-lg bg-orange-600 text-white py-2 px-4 ">
+            همین حالا سفارش دهید
+          </button>
+          <button className="font-bold border border-orange-600 text-orange-600 py-2 px-4 rounded-lg">
+               <Link to='/foods'>مشاهده همه غذاها</Link>
+          </button>
+        </div>
+        <div className="flex gap-16">
+          <p className="flex gap-2 items-center">
+            <span className="p-1 inline-block bg-orange-600 text-white rounded-full">
+              <FaShieldHalved />
+            </span>
+            <span>پرداختی ایمن</span>
+          </p>
+          <p className="flex gap-1 items-center">
+            <span className="p-1 inline-block bg-orange-600 text-white rounded-full">
+              <FaCar />
+            </span>
+            <span>پیک رایگان</span>
+          </p>
+        </div>
+      </div>
+      <div>
+        <img src={heroImg} alt="hero" />
+      </div>
+    </section>
+  );
 }
 
-export default Home
+export default Home;
