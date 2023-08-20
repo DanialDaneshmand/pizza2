42bbc288a1758468412c08eac3782164cb7d5c94
danial daneshmand <daniald11111377@gmail.com>
2023-08-20 07:05:37 -0700

    end of project

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Contact() {
   return (
-    <section className='py-32 flex justify-center'>
+    <section className='py-48 flex justify-center'>
         <p className='text-4xl '>this is contact page</p>
     </section>
   )
